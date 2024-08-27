7fd922c43dc7a5dbbd66f8465060005c8f577ed9
amish12822 <amishofficial23@gmail.com>
2024-08-27 21:34:40 +0530

    complete

---
+++ 1.print_sum.py
@@ -0,0 +1,6 @@
+"Write a Program to input 2 numbers & print their sum."
+
+a = int(input("Enter first value "))
+b = int(input("Enter second value "))
+
+print("Sum:",a+b)
---
+++ 2.square.py
@@ -0,0 +1,4 @@
+"WAP to input side of a square & print its area."
+
+side = int(input("Enter side value: "))
+print("Area of square:", side**2)
